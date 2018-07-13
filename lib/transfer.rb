d1f5dc8561f9feb29666dba1b61e736c539c2dcb
Jonathan Hong <jonhong777@gmail.com>
2018-07-13 22:10:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,6 +22,7 @@ class Transfer
        sender.balance -= self.amount
        receiver.balance += self.amount
        self.status = 'complete'
-    end
+    else
+      'Transaction rejected. Please check your account balance.'
   end 
 end
