3592e7f4b6bdf11000ea6c777c469aab911f46c1
Jonathan Hong <jonhong777@gmail.com>
2018-07-13 22:02:26 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,8 +19,7 @@ class Transfer
   
   def execute_transaction
     if !self.valid?
-      @status = 'rejected'
-      "Transaction rejected. Please check your account balance."
+      @status = "Transaction rejected. Please check your account balance."
     elsif self.status == 'pending'
        sender.balance -= self.amount
        receiver.balance += self.amount
